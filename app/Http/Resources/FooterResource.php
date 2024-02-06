d2f5cddc6cd388bfb29d5a6f573013a87cbdc99e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-07 03:38:07 +0700

    integration resources - Footer

---
+++ app/Http/Resources/FooterResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FooterResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'platform' => $this->platform,
+            'nama_halaman' => $this->nama_halaman,
+            'link_url' => $this->link_url,
+        ];
+    }
+}
