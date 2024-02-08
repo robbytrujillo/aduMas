f9281e7d89acf2729d445888b8f4690065a733cf
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-09 05:44:01 +0700

    integration policy - Aduan

---
+++ app/Policies/AduanPolicy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Aduan;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AduanPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
