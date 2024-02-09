f7f6b63d13efb5439f3bd4679508923cd7545571
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-10 05:34:09 +0700

    integration Service - Footer

--- app/Services/FooterService.php
+++ app/Services/FooterService.php
@@ -29,13 +29,45 @@ class FooterService {
                 'nama_halaman' => $twitterFooter->nama_halaman,
                 'link_url' => $twitterFooter->link_url
             ],
-            'twitter' => [
-                'platform' =>  $twitterFooter->platform,
-                'nama_halaman' => $twitterFooter->nama_halaman,
-                'link_url' => $twitterFooter->link_url
+            'instagram' => [
+                'platform' =>  $instagramFooter->platform,
+                'nama_halaman' => $instagramFooter->nama_halaman,
+                'link_url' => $instagramFooter->link_url
+            ],
+            'whatsapp' => [
+                'platform' =>  $whatsappFooter->platform,
+                'nama_halaman' => $whatsappFooter->nama_halaman,
+                'link_url' => $whatsappFooter->link_url
+            ],
+            'email' => [
+                'platform' =>  $emailFooter->platform,
+                'nama_halaman' => $emailFooter->nama_halaman,
+                'link_url' => $emailFooter->link_url
+            ],
+            'phone' => [
+                'platform' =>  $phoneFooter->platform,
+                'nama_halaman' => $phoneFooter->nama_halaman,
+                'link_url' => $phoneFooter->link_url
+            ],
+            'address' => [
+                'platform' =>  $addressFooter->platform,
+                'nama_halaman' => $addressFooter->nama_halaman,
+                'link_url' => $addressFooter->link_url
+            ],
+            'faqs' => [
+                'platform' =>  $faqsFooter->platform,
+                'nama_halaman' => $faqsFooter->nama_halaman,
+                'link_url' => $faqsFooter->link_url
+            ],
+            'privacy' => [
+                'platform' =>  $privacyFooter->platform,
+                'nama_halaman' => $privacyFooter->nama_halaman,
+                'link_url' => $privacyFooter->link_url
             ],
 
-        ]
+        ];
+
+        return $footerData;
 
     }
 }
