492725eee68e2726e211c22a6a20bb57325005f5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-12 11:41:19 +0700

    integration Controller 1

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -15,5 +15,63 @@ class DashboardController extends Controller
     public function index(): Response {
         $totalAduan = Aduan::count();
         $totalUser = User::count();
+        $totalPost = Post::count();
+        $aduanByDesa = Aduan::groupBy('desa_kelurahan')
+            ->select('desa_kelurahan', \DB::raw('count(*) as count'))
+            ->pluck('count', 'desa_kelurahan');
+
+        $backgroundColorDesa = [
+            #eff0f2;
+            #228764;
+            #ffe0e6;
+            #cdebff;
+            #e248be;
+            #fff3d6;
+        ];
+
+        $chartData = [
+            'label' => $aduanByDesa->keys()->all(),
+            'datasets' => [
+                [
+                    'label' => 'Total',
+                    'barThickness' => 40,
+                    'backgroundColor' => $backgroundColorDesa,
+                    'data' => $aduanByDesa->values()->all(),
+                ],
+            ],
+        ];
+
+        $aduanByKecamatan = Aduan::groupBy('kecamatan')
+            ->select('kecamatan', \DB::raw('count(*) as count'))
+            ->pluck('count', 'kecamatan');
+
+        $backgroundColorKecamatan = [
+            #eff0f2;
+            #228764;
+            #ffe0e6;
+            #cdebff;
+            #e248be;
+            #fff3d6;
+        ];
+
+        $chartDataKecamatan = [
+            'label' => $aduanByKecamatan->keys()->all(),
+            'datasets' => [
+                [
+                    'label' => 'Total',
+                    'barThickness' => 40,
+                    'backgroundColor' => $backgroundColorKecamatan,
+                    'data' => $aduanByKecamatan->values()->all(),
+                ],
+            ],
+        ];
+
+        return Inertia::render('Admin/AdminIndex', [
+            'chartData' => $chartData,
+            'chartDataKecamatan' => $chartDataKecamatan,
+            'totalDataAduan' => $totalAduan,
+            'totalDataUser' => $totalUser,
+            'totalDataPost' => $totalPost,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -25,14 +26,18 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'role:admin|moderator'])->prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.index');
 });
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
