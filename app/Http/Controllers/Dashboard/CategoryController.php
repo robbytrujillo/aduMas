8392d8a0703da9032a8bb0b337714d2fb16554d3
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-14 04:43:29 +0700

    integration Controller 2 | Dashboards - Category

---
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    //
+}
