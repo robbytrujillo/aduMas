d893e6e5dc9dc049380bf19e2f84026cd08971be
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-14 16:17:03 +0700

    integration Controller 2 | Dashboards - Category

--- app/Http/Controllers/Dashboard/CategoryController.php
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -17,6 +17,7 @@ class CategoryController extends Controller
 {
     public function index(): Response {
         $perPage = request('perPage') ?: 5;
+
         $categories = Category::query()
             ->when(request('search'), function($query, $search) {
                 $query->where('name', 'like', "%{$search}%");
@@ -24,5 +25,33 @@ class CategoryController extends Controller
             ->paginate($perPage)
             ->appends(request()->query());
 
+        $categories = CategoryResponse::collection($categories);
+        
+        $filters = request()->only(['search', 'perPage']);
+
+        return Inertia::render('Admin/Categories/CategoryIndex', compact('categories', 'filters'));
+    }
+
+    public function create(): Response {
+        $this->authorize('create', Category::class);
+        return Inertia::render('Admin/Categories/Create');
+    }
+
+    public function store(StoreCategoryRequest $request) {
+        $this->authorize('create', Category::class);
+        $image = $request->file('image');
+        $image->storeAs('pubic/categories', $image->hashName());
+
+        $category = Category::create([
+            'image' => $image->hashName(),
+            'name' => $request->name,
+            'slug' => Str::slug($request->name, '-'),
+        ]);
+
+        $category->save();
+        if ($category) {
+            // return success with Api Resource
+            return redirect()->route('categories.index');
+        }
     }
 }
