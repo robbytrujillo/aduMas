d38537bef2a0756409940d3283f3eab68adcfd1b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-18 07:48:48 +0700

    integration Controller 7 | dashboard - Post

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -5,6 +5,21 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\TagResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Request as Requests;
+use Inertia\Response;
+use Illuminate\Support\Str;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use App\Services\FooterService;
+
 class PostController extends Controller
 {
     //
