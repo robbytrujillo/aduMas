73320c244ca1a573e7cd6469c78b4b5a5108bc49
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-11 09:35:52 +0700

    Implement Components-Frontend | Form

--- resources/js/Components/Frontend/Form.vue
+++ resources/js/Components/Frontend/Form.vue
@@ -102,6 +102,31 @@ const imageUrl = computed(() => {
                             <input v-model="form.title" class="w-full p-3 text-sm border-pink-600 rounded-lg focus:ring-pink-500 focus:border-pink-500" type="text" placeholder="Jalan Rusak, Layanan kurang bagus" id="name" />
                                 <InputError class="mt-2" :message="form.errors.title" />
                         </div>
+                        <div class="sm:col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Aduan</label>
+                            <Multiselect class="rounded-lg border-pink-300 focus:ring-pink-500 focus:boder-pink-500" id="tags" v-model="selected" :options="dataTags" :multiple="true" :close-on-select="true"
+                            :group-select="true" placeholder="Type to search" label="name" track-by="id" />
+                                <InputError class="mt-2" :message="form.errors.tags" />
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Lampiran Aduan</label>
+                            <label for="image" value="Image" class="flex flex-col items-center justify-center w-full h-64 border-2 border-pink-600 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:hover:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                    </svg>
+                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                        <span class="font-semibold">Click to upload</span> or drag and drop
+                                    </p>
+                                    <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                </div>
+                                <input id="image" type="file" class="hidden mt-1 block w-full" />
+                                <img :src="imageUrl" class="h-25 w-20 object-contain" v-if="imageUrl" />
+                            </label>
+                            <InputError class="mt-2" :message="form.errors.image" />
+                        </div>
                     </div>
                 </form>
         </div>
