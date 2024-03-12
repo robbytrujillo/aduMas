e04ad4af3b61580da10695e05ae592ad4f2b2dc5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-12 14:31:22 +0700

    Implement Components-Frontend | Post By Category

---
+++ resources/js/Components/Frontend/PostByCategory.vue
@@ -0,0 +1,106 @@
+<script setup>
+import { defineProps } from "vue";
+import { Link, Head } from "@inertiajs/vue3";
+const props = defineProps({
+    posts: Object,
+    category: Object,
+    postCount: Object,
+});
+
+const formatStatusCreatedAt = (createdAt) => {
+    const date = new Date(createdAt);
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZoneName: "short",
+    };
+};
+
+const truncateText = (text) => {
+    const maxCharacters = 100;
+    return text.length > maxCharacters
+        ? text.substring(0, maxCharacters) + "..."
+        : text;
+};
+</script>
+
+<template>
+    <Head :title="category.name" />
+    <div class="col-span-3 sm:px-16 sm:py-6 py-4 grid">
+        <div class="bg-white shadow mt-10">
+            <!-- form header card -->
+            <div class="flex flex-wrap">
+                <div class="sm:w-full mb-3 sm:mb-3">
+                    <p class="font-semibold text-base">
+                        Post berdasarkan category
+                        <span
+                            class="rounded 3xl border border-slate-400 px-2 py-1"
+                            style="margin: 0.2rem"
+                        >
+                            #{{ category.name }}
+                        </span>
+                    </p>
+                    <p class="text-slate-500 text-sm font-light">
+                        Terdapat {{ postCount }} total post
+                    </p>
+                </div>
+            </div>
+
+            <!-- List of all post -->
+            <div class="card-body px-3">
+                <template v-if="posts.data && postcss.data.length > 0">
+                    <div
+                        v-for="(post, index) in posts.data"
+                        :key="index"
+                        class="post flex sm:flex-nowrap flex-wrap border-b border-slate-200 px-3 py-4 font-inter"
+                    >
+                        <div class="sm:flex-none sm:w-36 sm:h-fit w-full">
+                            <Link
+                                :href="
+                                    route('posts.detail', { slug: post.slug })
+                                "
+                            >
+                                <img
+                                    :src="post.image"
+                                    class="object-cover sm:h-36 h-52 w-full"
+                                    alt="image card"
+                                />
+                            </Link>
+                        </div>
+                        <div
+                            class="sm:flex-initial sm:w-full grid content-between min-h-[9em] pl-3 sm:mt-0 mt-3"
+                        >
+                            <Link
+                                :href="
+                                    route('posts.detail', {
+                                        slug: postMessage.slug,
+                                    })
+                                "
+                                class="self-auto"
+                            >
+                                <p class="text-red-900 text-xl font-semibold">
+                                    {{ post.title }}
+                                </p>
+                                <p class="text-slate-500 text-sm font-light">
+                                    DESA {{ post.desa_kelurahan }},
+                                    {{
+                                        formatStatusCreatedAt(
+                                            postMessage.created_at
+                                        )
+                                    }}
+                                </p>
+                            </Link>
+                            <div class="">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </template>
+            </div>
+        </div>
+    </div>
+</template>
