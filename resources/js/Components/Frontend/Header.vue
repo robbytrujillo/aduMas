a487e117ef4d25f1190a2ec730d6f76740faa98d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-12 10:15:54 +0700

    Implement Components-Frontend | Header

--- resources/js/Components/Frontend/Header.vue
+++ resources/js/Components/Frontend/Header.vue
@@ -32,6 +32,30 @@ defineProps({
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-pink-600">Lapor</span>
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-black">Kab!</span>
                 </Link>
+                <div class="flex items-center lg:order-2">
+                    <div>
+                        <template v-if="$page.props.auth.user">
+                            <template v-if="$page.props.auth.user.roles.includes('user')">
+                                <Link :href="route('user.index')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:outline-red-500">
+                                    Dashboard (User)
+                                </Link>
+                            </template>
+                            <template v-else>
+                                <Link :href="route('admin.index')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:outline-red-500">
+                                    Dashboard (Admin/Moderator)
+                                </Link>
+                            </template>
+                        </template>
+                        <template v-else>
+                            <Link :href="route('login')" class="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400">
+                                Log In
+                            </Link>
+                        </template>
+                    </div>
+                    <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                            
+                    </button>
+                </div>
             </div>
         </nav>
     </header>
