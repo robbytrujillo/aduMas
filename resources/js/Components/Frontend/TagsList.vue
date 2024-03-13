91baeac9f08928645d5b287587b0b1488ae1eb01
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-13 14:37:58 +0700

    Implement Components-Frontend | Tags List

---
+++ resources/js/Components/Frontend/TagsList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from "vue";
+import { Link, Head } from "@inertiajs/vue3";
+
+const props = defineProps({
+    aduans: Object,
+    tag: Object,
+});
+
+const formatStatusCreatedAt = (createdAt) => {
+    const date = new Date(createdAt);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    return date.toLocaleDateString('id-ID', options);
+}
+</script>
+<template>
+    <Head title="Data Tags" />
+</template>
