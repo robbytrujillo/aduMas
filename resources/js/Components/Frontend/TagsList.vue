299f21f15589dbfe2ebab7e9ec632329f4c4f426
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-13 14:55:16 +0700

    Implement Components-Frontend | Tags List

--- resources/js/Components/Frontend/TagsList.vue
+++ resources/js/Components/Frontend/TagsList.vue
@@ -9,10 +9,35 @@ const props = defineProps({
 
 const formatStatusCreatedAt = (createdAt) => {
     const date = new Date(createdAt);
-    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-    return date.toLocaleDateString('id-ID', options);
-}
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZoneName: "short",
+    };
+    return date.toLocaleDateString("id-ID", options);
+};
 </script>
 <template>
     <Head title="Data Tags" />
+    <div class="col-span-3 sm:px-16 sm:py-6 py-4 grid">
+        <div class="bg-white shadow mt-10">
+            <!-- Form header card -->
+            <div class="flex flex-wrap p-4 border-b border-slate-200">
+                <div class="font-semibold text-base">
+                    Aduan berdasarkan tag
+                    <span
+                        class="rounded-3xl border border-slate-400 px-2 py-1"
+                        style="margin: 0.2rem"
+                    >
+                        #{{ Tags.name }}
+                    </span>
+                </div>
+            </div>
+            <!-- List of all aduan -->
+        </div>
+    </div>
 </template>
