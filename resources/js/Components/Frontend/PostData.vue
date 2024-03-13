2384b38c70d71bf707066f43fa0e8456dcef6ce9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-13 13:56:33 +0700

    Implement Components-Frontend | Post Data

---
+++ resources/js/Components/Frontend/PostData.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps, ref, computed } from "vue";
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    posts: Object,
+});
+
+const formatTitle = (title) => {
+    const maxLength = 20;
+    const formattedTitle = title.toLowerCase().replace(/\b\w/g, c => c.toUpperCase());
+
+    if (title.length > maxLength) {
+        return `${formattedTitle.substring(0, maxLength)}...`;
+    } else {
+        return formattedTitle;
+    }
+};
+const formattedTitle = (postTitle) => computed(() => formatTitle(postTitle));
+
+</script>
+
+<template>
+    <aside id="blogs" aria-label="Related articles" class="py-8 lg:py-24 bg-white dark:bg-gray-900">
+        <div class="px-4 mx-auto max-w-screen-xl">
+            <h2 class="">
+                
+            </h2>
+        </div>
+    </aside>
+</template>
