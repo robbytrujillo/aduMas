8422efe82bbd5325ee88464f1286635cd65a4c11
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-13 09:33:32 +0700

    Implement Components-Frontend | Post By Tag

--- resources/js/Components/Frontend/PostByTag.vue
+++ resources/js/Components/Frontend/PostByTag.vue
@@ -53,39 +53,85 @@ const truncateText = (text) => {
             <!-- List of all post -->
             <div class="card-body px-3">
                 <template v-if="posts.data && posts.data.length > 0">
-                    <div v-for="(post, index) in posts.data " :key="index" class="post flex sm:flex-nowrap flex-wrap border-b border-slate-200 px-3 py-4 font-inter">
+                    <div
+                        v-for="(post, index) in posts.data"
+                        :key="index"
+                        class="post flex sm:flex-nowrap flex-wrap border-b border-slate-200 px-3 py-4 font-inter"
+                    >
                         <div class="sm:flex-none sm:w-36 sm:h-fit w-full">
-                            <Link :href="route('posts.detail', { slug: post.slug })">
-                                <img :src="post.image" class="object-cover sm:h-36 h-52 w-full" alt="image card">
+                            <Link
+                                :href="
+                                    route('posts.detail', { slug: post.slug })
+                                "
+                            >
+                                <img
+                                    :src="post.image"
+                                    class="object-cover sm:h-36 h-52 w-full"
+                                    alt="image card"
+                                />
                             </Link>
                         </div>
-                        <div class="sm:flex-initial sm:w-full grid content-between min-h-[9em] pl-3 sm:mt-0 mt-3">
-                            <Link :href="route('posts.detail', {slug:post.slug})" class="self-auto">
+                        <div
+                            class="sm:flex-initial sm:w-full grid content-between min-h-[9em] pl-3 sm:mt-0 mt-3"
+                        >
+                            <Link
+                                :href="
+                                    route('posts.detail', { slug: post.slug })
+                                "
+                                class="self-auto"
+                            >
                                 <p class="text-red-900 text-xl font-semibold">
                                     {{ post.title }}
                                 </p>
-                                <p class="text-slate-500 text-sm text-sm font-light">DESA {{ post.desa_kelurahan }}, {{ formatStatusCreatedAt(postMessage.created_at) }}</p>
-                                <p class="py-3 text-sm" v-html="truncateText(postMessage.content)"></p>
+                                <p
+                                    class="text-slate-500 text-sm text-sm font-light"
+                                >
+                                    DESA {{ post.desa_kelurahan }},
+                                    {{
+                                        formatStatusCreatedAt(
+                                            postMessage.created_at
+                                        )
+                                    }}
+                                </p>
+                                <p
+                                    class="py-3 text-sm"
+                                    v-html="truncateText(postMessage.content)"
+                                ></p>
                             </Link>
                             <div class="grid grid-cols-3">
                                 <div class="col-span-3 sm:col-span-1">
-                                    <span class="rounded-full border text-red-600 px-2 py-1 sm:px-2 sm:py-1 text-sm" style="background-color: #ddf2fe; color: #2d9cdb;">
+                                    <span
+                                        class="rounded-full border text-red-600 px-2 py-1 sm:px-2 sm:py-1 text-sm"
+                                        style="
+                                            background-color: #ddf2fe;
+                                            color: #2d9cdb;
+                                        "
+                                    >
                                         Dilihat ({ post.views_count }) kali
                                     </span>
                                 </div>
-                                <div class="col-span-3 sm:col-span-2 sm:justify-self-end flex justify-end pt-4">
-                                    <span class="text-gray-400 text-xs font-light mr-2">
-                                        <span id="count-mark-152887">Author:</span> {{ post.user.name }}
+                                <div
+                                    class="col-span-3 sm:col-span-2 sm:justify-self-end flex justify-end pt-4"
+                                >
+                                    <span
+                                        class="text-gray-400 text-xs font-light mr-2"
+                                    >
+                                        <span id="count-mark-152887"
+                                            >Author:</span
+                                        >
+                                        {{ post.user.name }}
                                     </span>
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </template>
                 <template v-else>
-                    <p class="text-center text-gray-500 text-lg font-semibold">Maaf, data post berdasarkan tag yang dipilih masih kosong.</p>
-                </template> 
+                    <p class="text-center text-gray-500 text-lg font-semibold">
+                        Maaf, data post berdasarkan tag yang dipilih masih
+                        kosong.
+                    </p>
+                </template>
             </div>
         </div>
     </div>
